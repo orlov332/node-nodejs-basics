bf210223340c51a90e52b2d806f397fbfbf9844c
orlov332 <orlov332@gmail.com>
2022-06-02 16:55:23 +0200

    feat: Implement fs create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import { writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  const file = `${__dirname}/files/fresh.txt`;
+  const data = 'I am fresh and young';
+
+  return writeFile(file, data, { flag: 'wx' }).catch((err) => {
+    if (err.code === 'EEXIST') throw new Error('FS operation failed');
+    else throw err;
+  });
 };
+
+await create();
