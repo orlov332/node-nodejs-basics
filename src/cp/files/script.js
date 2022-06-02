0dd326b75c4f8544550f275e44e98bdad33f12b7
orlov332 <orlov332@gmail.com>
2022-06-02 16:51:14 +0200

    refactor: Add linter and formatter

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -4,9 +4,9 @@ console.log(`Total number of arguments is ${arguments.length}`);
 console.log(`Arguments: ${JSON.stringify(arguments)}`);
 
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  const chunkStringified = chunk.toString();
+  if (chunkStringified.includes("CLOSE")) process.exit(0);
+  process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
 };
 
-process.stdin.on('data', echoInput);
+process.stdin.on("data", echoInput);
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,3 @@
 export const compress = async () => {
-    // Write your code here 
+  // Write your code here
 };
