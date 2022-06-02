5c7aa2bf5274ac08aecc61ec6ed37f742495cbd6
orlov332 <orlov332@gmail.com>
2022-06-02 23:06:34 +0200

    feat: Implement parse command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-  // Write your code here
+  const appArgs = process.argv.slice(2);
+  console.log(
+    appArgs
+      .reduce((res, _, i) => {
+        if (i % 2 === 0) res.push(appArgs.slice(i, i + 2));
+        return res;
+      }, [])
+      .map(([name, value]) => `${name.replace('--', '')} is ${value}`)
+      .join(', ')
+  );
 };
+
+parseArgs();
