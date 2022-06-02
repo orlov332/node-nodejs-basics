41904605f347f98650423950843085cb071ddde5
orlov332 <orlov332@gmail.com>
2022-06-02 21:52:18 +0200

    feat: Implement fs list directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fsPromises from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const list = async () => {
-  // Write your code here
+  const dir = `${__dirname}/files`;
+
+  return fsPromises
+    .readdir(dir)
+    .catch((err) => {
+      if (err.code === 'ENOENT') throw new Error('FS operation failed');
+      else throw err;
+    })
+    .then((files) => files.forEach((file) => console.log(file)));
 };
+
+await list();
