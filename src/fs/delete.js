502989dce4ae06480073ced7442c189a89e8f307
orlov332 <orlov332@gmail.com>
2022-06-02 21:42:29 +0200

    feat: Implement fs delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fsPromises from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-  // Write your code here
+  const file = `${__dirname}/files/fileToRemove.txt`;
+
+  return fsPromises.rm(file).catch((err) => {
+    if (err.code === 'ENOENT') throw new Error('FS operation failed');
+    else throw err;
+  });
 };
+
+await remove();
