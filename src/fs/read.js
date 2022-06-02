d49932a145747b2ac761bb45707a19b0a59f7e61
orlov332 <orlov332@gmail.com>
2022-06-02 22:05:41 +0200

    feat: Implement fs read file content

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fsPromises from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-  // Write your code here
+  const file = `${__dirname}/files/fileToRead.txt`;
+
+  return fsPromises
+    .readFile(file, 'utf8')
+    .catch((err) => {
+      if (err.code === 'ENOENT') throw new Error('FS operation failed');
+      else throw err;
+    })
+    .then(console.log);
 };
+
+await read();
