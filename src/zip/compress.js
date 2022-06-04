703d37c1209a3669be3880cd9b88c16cef548517
orlov332 <orlov332@gmail.com>
2022-06-04 14:12:18 +0200

    feat: Implement zip/unzip file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const compress = async () => {
-  // Write your code here
+  return pipeline(
+    fs.createReadStream(`${__dirname}/files/fileToCompress.txt`),
+    zlib.createGzip(),
+    fs.createWriteStream(`${__dirname}/files/archive.gz`)
+  );
 };
+
+await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const decompress = async () => {
-  // Write your code here
+  return pipeline(
+    fs.createReadStream(`${__dirname}/files/archive.gz`),
+    zlib.createGunzip(),
+    fs.createWriteStream(`${__dirname}/files/fileToCompress.txt`)
+  );
 };
+
+await decompress();
