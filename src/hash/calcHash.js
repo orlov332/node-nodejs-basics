f9d1287d844f322b2fb443b4f1224db431fa9d0c
orlov332 <orlov332@gmail.com>
2022-06-04 11:03:09 +0200

    feat: Implement file hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fsPromises from 'fs/promises';
+import crypto from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-  // Write your code here
+  const file = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+  return fsPromises
+    .readFile(file)
+    .then((content) =>
+      crypto.createHash('sha256').update(content).digest('hex')
+    );
 };
+
+console.log(await calculateHash());
