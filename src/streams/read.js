34cb708a422430ae765e422de8cbe9e7070255e2
orlov332 <orlov332@gmail.com>
2022-06-04 12:02:47 +0200

    feat: Implement stream file read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-  // Write your code here
+  return pipeline(
+    fs.createReadStream(`${__dirname}/files/fileToRead.txt`, 'utf8'),
+    process.stdout
+  );
 };
+
+await read();
