fe4da3165351933b02ed38a0be5de69a74510134
orlov332 <orlov332@gmail.com>
2022-06-04 21:26:44 +0200

    feat: Implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,22 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 export const performCalculations = async () => {
-  // Write your code here
+  return Promise.all(
+    Array.from({ length: os.cpus().length }, (v, i) => 10 + i).map((num) =>
+      new Promise((resolve, reject) => {
+        const worker = new Worker('./worker.js', { workerData: num });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        worker.on('exit', (code) => {
+          if (code !== 0)
+            reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+      })
+        .then((data) => ({ status: 'resolved', data }))
+        .catch(() => ({ status: 'error', data: null }))
+    )
+  );
 };
+
+await performCalculations().then(console.log);
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,11 @@
+import { isMainThread, parentPort, workerData } from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(nthFibonacci(workerData));
 };
+
+if (!isMainThread) sendResult();
