628dd66d9d82e6009ce23fbdd7ed32d106e52029
orlov332 <orlov332@gmail.com>
2022-06-04 22:45:10 +0200

    feat: Implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-  // Write your code here
+  const cp = spawn('node', [`${__dirname}/files/script.js`, ...args]);
+
+  return Promise.all([
+    pipeline(cp.stdout, process.stdout),
+    pipeline(process.stdin, cp.stdin),
+  ]);
 };
+
+await spawnChildProcess(process.argv.slice(2));
