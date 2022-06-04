54228b72c8aa0ae502bf33d8cd13b5093504cf1f
orlov332 <orlov332@gmail.com>
2022-06-04 12:28:55 +0200

    feat: Implement stream file write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const write = async () => {
-  // Write your code here
+  return pipeline(
+    process.stdin,
+    fs.createWriteStream(`${__dirname}/files/fileToWrite.txt`, 'utf8')
+  );
 };
+
+await write();
